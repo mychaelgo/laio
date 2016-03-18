e87c85f85a6565db0a01ccce531147af3508643f
mychaelgo <admin@mychaelgo.com>
2016-03-18 21:16:56 +0700

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup setup(
+  name = 'laoi',
+  packages = ['laoi], # this must be the same as the name above
+  version = '0.1',
+  description = 'Laio (Localize Android iOS)',
+  author = 'Mychael Go',
+  author_email = 'admin@mychaelgo.com',
+  url = 'https://github.com/mychaelgo/laio', # use the URL to the github repo
+  download_url = 'https://github.com/mychaelgo/laoi/tarball/0.1', # I'll explain this in a second
+  keywords = ['localize', 'android', 'ios'], # arbitrary keywords
+  classifiers = [],
+)
